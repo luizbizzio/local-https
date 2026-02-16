c120ac04383bbf76c101dfdc9f096fc48473735b
Luiz Bizzio <contact@luizbizzio.com>
2026-02-16 18:34:34 +0100

    Add license and copyright comments
    Add license information and copyright notice to install.sh
    
    Signed-off-by: Luiz Bizzio <contact@luizbizzio.com>

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2024-2026 Luiz Bizzio
 set -euo pipefail
 
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
